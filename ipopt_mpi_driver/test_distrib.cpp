ad1bfba18f45dd58e914ed9f4f51adfc4afe8a0b
Felix Liu <felix.shi.liu@gmail.com>
2022-03-28 17:40:12 +0200

    Added tests

---
+++ ipopt_mpi_driver/test_distrib.cpp
@@ -0,0 +1,41 @@
+#include "test_distrib.h"
+#include "TROTSEntry.h"
+
+#include "debug_utils.h"
+
+#include <iostream>
+
+void test_trotsentry_distrib(const LocalData& local_data,
+                             const std::vector<std::vector<int>>* distrib_ptr,
+                             const std::vector<TROTSEntry>* entries,
+                             MPI_Comm comm) {
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+    int size;
+    MPI_Comm_size(comm, &size);
+    std::vector<int> data_ids;
+    if (rank == 0) {
+        assert(distrib_ptr != nullptr && entries != nullptr);
+        std::vector<std::vector<int>> distrib = *distrib_ptr;
+        assert(size == static_cast<int>(distrib.size()));
+        for (int other_rank = 1; other_rank < distrib.size(); ++other_rank) {
+            const std::vector<int>& idxs = distrib[other_rank];
+            for (int idx : idxs) {
+                data_ids.push_back((*entries)[idx].get_id());
+            }
+            MPI_Send(&data_ids[0], data_ids.size(), MPI_INT, other_rank, 0, comm);
+            data_ids.clear();
+        }
+    }
+    else {
+        std::vector<int> local_data_ids;
+        for (int i = 0; i < local_data.trots_entries.size(); ++i) {
+            local_data_ids.push_back(local_data.trots_entries[i].get_id());
+        }
+        data_ids.resize(local_data_ids.size());
+        MPI_Recv(&data_ids[0], data_ids.size(), MPI_INT, 0, 0, comm, MPI_STATUS_IGNORE);
+        for (int i = 0; i < local_data_ids.size(); ++i) {
+            assert(data_ids[i] == local_data_ids[i]);
+        }
+    }
+}
---
+++ ipopt_mpi_driver/test_distrib.h
@@ -0,0 +1,13 @@
+#ifndef TEST_DISTRIB_H
+#define TEST_DISTRIB_H
+
+#include <vector>
+#include <mpi.h>
+#include "rank_local_data.h"
+
+void test_trotsentry_distrib(const LocalData& local_data,
+                             const std::vector<std::vector<int>>* entry_distrib,
+                             const std::vector<TROTSEntry>* entries,
+                             MPI_Comm comm);
+
+#endif
